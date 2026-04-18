49b3f5524e65888eb7deffb65f5615a1511c0d85
mithrandir3010 <malibulut2@icloud.com>
2026-04-18 15:26:47 +0300

    refactor: implement standard layered architecture (controller-service-repository)

--- src/main/java/com/mali/smartbudget/controller/HelloController.java
+++ src/main/java/com/mali/smartbudget/controller/HelloController.java
@@ -1,4 +1,4 @@
-package com.mali.smartbudget;
+package com.mali.smartbudget.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
